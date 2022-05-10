858eecac65d0f33e7fcc5d12fa34d1740b0593db
Hasibur Rahman <hasiburdev@gmail.com>
2022-05-11 02:26:08 +0600

    Set up boilerplate server code

---
+++ server/models/dbModels/productModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+export default mongoose.model("Product", productSchema);
