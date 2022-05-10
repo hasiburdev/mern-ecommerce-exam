21115fd3f9c9442d7d23d054c17b5c4de5cdd0bf
Hasibur Rahman <hasiburdev@gmail.com>
2022-05-11 02:36:59 +0600

    Removed the MongoDB URI from code

--- server/seed.js
+++ server/seed.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 import User from "./models/dbModels/userModel.js";
+const DB_URL = process.env.MONGO_DB_URL;
 
-mongoose.connect(
-  "mongodb+srv://hasib:hasib@cluster0.em0mf.mongodb.net/mernEcommerceTest?retryWrites=true&w=majority",
-  async () => {
-    const user = new User({
-      name: "Hasib",
-      password: "123456",
-      email: "hasib@gamil.com",
-    });
-    const response = await user.save();
-    console.log(response);
-  }
-);
+mongoose.connect(DB_URL, async () => {
+  const user = new User({
+    name: "Hasib",
+    password: "123456",
+    email: "hasib@gamil.com",
+  });
+  const response = await user.save();
+  console.log(response);
+});
